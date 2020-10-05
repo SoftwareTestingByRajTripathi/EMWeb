9aa2a4d618fdc1a7df3cb0ca393b458c5831cd68
SoftwareTestingByRajTripathi <tripathideva@gmail.com>
2020-10-05 19:16:52 +0530

    Commit Studypage

---
+++ EMWeb/src/test/java/com/emwebpage/EMWebStudyPage.java
@@ -0,0 +1,35 @@
+package com.emwebpage;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class EMWebStudyPage {
+	WebDriver driver;
+	@FindBy(xpath="//a[contains(text(),'Study')]") WebElement study;
+	
+	EMWebStudyPage (WebDriver driver)
+	{
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	public void Studypage()
+	{
+		study.click();
+	}
+	public void chapterList()
+	{
+		List<WebElement> chapterlist = driver.findElements(By.xpath("//a[@class ='subject-list']"));
+		System.out.println(chapterlist.size());
+		for (int i =0; i<chapterlist.size();i++)
+		{
+		String linktext = 	chapterlist.get(i).getText();
+		System.out.println(chapterlist);
+		}
+	}
+
+}
