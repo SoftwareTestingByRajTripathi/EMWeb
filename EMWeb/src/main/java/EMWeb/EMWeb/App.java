cae244e3743d6de4ca7ae7d4c2455a08fbbabb0e
SoftwareTestingByRajTripathi <tripathideva@gmail.com>
2020-10-05 13:32:27 +0530

    Fisrtcommint by raj

---
+++ EMWeb/src/main/java/EMWeb/EMWeb/App.java
@@ -0,0 +1,13 @@
+package EMWeb.EMWeb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
