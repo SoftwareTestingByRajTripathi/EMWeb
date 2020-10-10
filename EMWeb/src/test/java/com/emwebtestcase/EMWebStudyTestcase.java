7f1503148896e3e1ed80d4ebd34859968c7f7e99
SoftwareTestingByRajTripathi <tripathideva@gmail.com>
2020-10-10 10:23:38 +0530

    Commit - Waits

--- EMWeb/src/test/java/com/emwebtestcase/EMWebStudyTestcase.java
+++ EMWeb/src/test/java/com/emwebtestcase/EMWebStudyTestcase.java
@@ -8,14 +8,18 @@ public class EMWebStudyTestcase extends EMWebBasePage{
 	EMWebLoginPage EMlogin;
 	EMWebStudyPage EWStudy;
 	//For Count the chapter list of Subject//
-	public void CountChapterList_studyPage()
+	public void CountChapterList_studyPage() throws InterruptedException
 	{
 		EMlogin = new EMWebLoginPage(driver);
 		EMlogin.clickonLoginbtn();
+		Thread.sleep(3000);
 		EMlogin.dhgc();
+		Thread.sleep(3000);
 		EMlogin.useridandPass("9136342965", "ADVIK@2019");
 		EWStudy = new EMWebStudyPage(driver);
+		Thread.sleep(3000);
 		EWStudy.Studypage();
+		Thread.sleep(3000);
 		EWStudy.chapterList();
 		
 		
